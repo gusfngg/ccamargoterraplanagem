dc46452174a9a5bd271f7bda723fb9d72c11ad1c
Gustavo Camargo <gus@MacBook-Air-de-Gustavo.local>
2025-11-20 18:15:29 -0300

    fix google tag

--- src/components/ui/button-cta.tsx
+++ src/components/ui/button-cta.tsx
@@ -7,18 +7,25 @@ interface ButtonCtaInterface {
 }
 
 export function ButtonCta({ message, className }: ButtonCtaInterface) {
+  const handleClick = () => {
+    if (typeof window !== 'undefined') {
+      // dispara o evento
+      // sem redirecionamento porque você já usa <Link>
+      window.gtag('event', 'conversion', {
+        send_to: 'AW-16626951404/1FXaCJuL9cMbEOzJrPg9',
+      });
+    }
+  };
+
   return (
     <Link
-      href={
-        'https://api.whatsapp.com/send?phone=5515996593858&text=Quero%20solicitar%20um%20or%C3%A7amento!'
-      }
+      href="https://api.whatsapp.com/send?phone=5515996593858&text=Quero%20solicitar%20um%20or%C3%A7amento!"
       target="_blank"
+      onClick={handleClick}
     >
-      <Button
-        className={`${className} bg-blueAm text-grayAm font-semibold hover:bg-blue-900 `}
-      >
+      <Button className={`${className} bg-blueAm text-grayAm font-semibold hover:bg-blue-900 `}>
         {message}
       </Button>
     </Link>
-  )
+  );
 }
